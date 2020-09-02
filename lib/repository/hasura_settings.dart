f809e6ce3279488c8c90c0cb11b26e0832034587
matheuswells <matheuswt0@gmail.com>
2020-09-02 20:21:26 -0300

    New Readme and Fixes

--- lib/repository/hasura_settings.dart
+++ lib/repository/hasura_settings.dart
@@ -1,8 +1,9 @@
 import 'package:get/get.dart';
 import 'package:hasura_connect/hasura_connect.dart';
 
-class HasuraSettings extends GetxController{
+class HasuraSettings extends GetxController {
   static HasuraSettings get to => Get.find();
 
-  HasuraConnect connect = HasuraConnect("API-KEY");
+  HasuraConnect connect =
+      HasuraConnect("https://resolved-parrot-56.hasura.app/v1/graphql");
 }
