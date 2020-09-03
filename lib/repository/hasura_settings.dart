3697dda22c3a674a06352a6ece3297dcd6f6577e
matheuswells <matheuswt0@gmail.com>
2020-09-02 21:04:43 -0300

    Private API key removed

--- lib/repository/hasura_settings.dart
+++ lib/repository/hasura_settings.dart
@@ -4,6 +4,5 @@ import 'package:hasura_connect/hasura_connect.dart';
 class HasuraSettings extends GetxController {
   static HasuraSettings get to => Get.find();
 
-  HasuraConnect connect =
-      HasuraConnect("https://resolved-parrot-56.hasura.app/v1/graphql");
+  HasuraConnect connect = HasuraConnect("API-KEY");
 }
